6b9fb904daef7596cc55255d04c18ab20b9c2ce0
SantiagoTrifonArevalo <santiagodtrifon@gmail.com>
2023-06-23 17:51:09 +0200

    create fils index and main

---
+++ main.js
@@ -0,0 +1,26 @@
+// strings
+// ejercicio.1
+let probando = "soy un noob";
+// ejercicio.2
+console.log(probando);
+probando = "No entiendo nada";
+console.log(probando);
+probando = "No entiendo nada";
+// ejercicio.3
+console.log("No entiendo nada".search("t"));
+// ejercicio.4
+probando = "No entiendo nada";
+probando[0]="a";
+console.log(probando) 
+// Arrays
+// ejercicio.1
+let classNames = ["Otniel","Morales", "Oscar", "Javi","Santiago"]
+// ejercicio.2
+console.log(classNames[4]);
+// ejercicio.3
+classNames[4]
+console.log(classNames);
+// ejercicio.4
+// Objetos
+// Crea un objeto con las key name, iLikeJs y favFilms -> que será un array de al menos 3 posiciones.
+// Todos deberán tener los valores apropiados.
